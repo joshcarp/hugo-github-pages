db0d639cbb9b35c2dbdfa65b3ee273a8a5d082d3
joshcarp <joshuacarpeggiani@gmail.com>
2020-02-01 08:01:17 +1100

    Change _source to _hugo

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 cd docs && rm -rf * 
 cd ..
-cd _source
+cd _hugo
 # Build the project.
 hugo -t reveal-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
